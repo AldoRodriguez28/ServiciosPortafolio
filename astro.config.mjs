c7e990927325558b99a587067705217c8291bb73
AldoRodriguez28 <arodriguez.ronquillo@hotmail.com>
2026-02-05 22:39:15 -0600

    agregando favicon

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import { createRequire } from 'module';
+
+const require = createRequire(import.meta.url);
 
 export default defineConfig({
     site: 'https://AldoRodriguez28.github.io',
-    base: '/ServiciosPortafolio',
-  })
+    base: '/ServiciosPortafolio/',
+  });
+
